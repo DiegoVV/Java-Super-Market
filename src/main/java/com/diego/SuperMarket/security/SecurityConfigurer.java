fb7422c26b19357a37f443f9865fdc5b2224e5ff
DiegoVV <diego.volkmann@agilesolutions.com>
2021-08-20 00:58:18 -0300

    very crude security override with user foo to establish groundwork for JWT authentication system

---
+++ src/main/java/com/diego/SuperMarket/security/SecurityConfigurer.java
@@ -0,0 +1,26 @@
+package com.diego.SuperMarket.security;
+
+import com.diego.SuperMarket.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
